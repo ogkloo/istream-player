e9e4318863847ae9dcc230ed0d09b4a42d2954aa
Sam <weathercontrol@tuta.io>
2024-02-12 13:52:31 -0800

    adds a package name field for later use

--- flake.nix
+++ flake.nix
@@ -5,8 +5,10 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        packageName = "istream-player";
       in
       {
+        #defaultPackage = self.packages.${system}.${packageName};
         devShell = pkgs.callPackage ./nix/devShell.nix { };
       }
     );
